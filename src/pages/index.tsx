d827a3fa253b2c912819dd10fc663958e71fec85
rice-hill <heavenhayao2316@gmail.com>
2022-07-20 10:59:04 +0900

    [add] Random cat images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,33 @@
+import { useState } from 'react'
+
+const catImages: string[] = [
+    "https://cdn2.thecatapi.com/images/bpc.jpg",
+    "https://cdn2.thecatapi.com/images/eac.jpg",
+    "https://cdn2.thecatapi.com/images/6qi.jpg",
+]
+
+const randomCatImage = (): string => {
+    const index = Math.floor(Math.random() * catImages.length)
+    return catImages[index]
+}
+
 const IndexPage = () => {
-    return <h1>Hello, Next.js 👋</h1>
+    const [catImageUrl, setCatImageUrl] = useState(
+        "https://cdn2.thecatapi.com/images/bpc.jpg"
+    )
+
+    const handleClick = () => {
+        setCatImageUrl(randomCatImage())
+    }
+
+    return (
+        <>
+            <button onClick={handleClick}>きょうのにゃんこ🐱</button>
+            <div style={{ marginTop: 8 }}>
+                <img src={catImageUrl} />
+            </div>
+        </>
+    )
 }
 
 export default IndexPage
