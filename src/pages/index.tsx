baad1ad7b7e8cb3f3763f75512bc8bbe1cd4b231
rice-hill <heavenhayao2316@gmail.com>
2022-07-20 11:19:05 +0900

    [add] Random cat images via API request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,40 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-const catImages: string[] = [
-    "https://cdn2.thecatapi.com/images/bpc.jpg",
-    "https://cdn2.thecatapi.com/images/eac.jpg",
-    "https://cdn2.thecatapi.com/images/6qi.jpg",
-]
-
-const randomCatImage = (): string => {
-    const index = Math.floor(Math.random() * catImages.length)
-    return catImages[index]
+interface CatCategory {
+    id: number
+    name: string
 }
+
+interface SearchCatImage {
+    breeds: string[]
+    categories: CatCategory
+    id: string
+    url: string
+    width: number
+    height: number
+}
+
+type SearchCatImageResponse = SearchCatImage[]
+
+
+
+const fetchCatImage = async (): Promise<SearchCatImage> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search")
+    const result = await res.json() as SearchCatImageResponse
+        // 「as」:TypeScriptには、型推論を上書きする機能があります。その機能を型アサーション(type assertion)と言います。
+    return result[0]
+}
+
+
 
 const IndexPage = () => {
     const [catImageUrl, setCatImageUrl] = useState(
         "https://cdn2.thecatapi.com/images/bpc.jpg"
     )
 
-    const handleClick = () => {
-        setCatImageUrl(randomCatImage())
+    const handleClick = async() => {
+        const image = await fetchCatImage()
+        setCatImageUrl(image.url)
     }
 
     return (
