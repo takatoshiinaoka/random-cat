71e47abfe6a4c422c43e6e2baefc46e5e8e61d69
rice-hill <heavenhayao2316@gmail.com>
2022-07-20 11:38:30 +0900

    [add] Display images even when refreshing pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import type { NextPage, GetServerSideProps, GetServerSideProps } from 'next'
 
 interface CatCategory {
     id: number
@@ -25,12 +26,12 @@ const fetchCatImage = async (): Promise<SearchCatImage> => {
     return result[0]
 }
 
+interface IndexPageProps {
+    initialCatImageUrl: string
+}
 
-
-const IndexPage = () => {
-    const [catImageUrl, setCatImageUrl] = useState(
-        "https://cdn2.thecatapi.com/images/bpc.jpg"
-    )
+const IndexPage: NextPage<IndexPageProps> = ({initialCatImageUrl}) => {
+    const [catImageUrl, setCatImageUrl] = useState(initialCatImageUrl)
 
     const handleClick = async() => {
         const image = await fetchCatImage()
@@ -45,6 +46,17 @@ const IndexPage = () => {
             </div>
         </>
     )
+}
+
+export const getServerSideProps: GetServerSideProps<
+    IndexPageProps
+> = async () => {
+    const catImage = await fetchCatImage()
+    return{
+        props: {
+            initialCatImageUrl: catImage.url
+        }
+    }
 }
 
 export default IndexPage
